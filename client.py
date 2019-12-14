958930c7d2487c8791555c88f9ece724676e2bf5
jtmaston <jtmstn1@gmail.com>
2019-12-15 01:13:27 +0200

    Cleanup
    Warning! While server and client are functional, they are SINGLE USE. Will update soon.

--- client.py
+++ client.py
@@ -1,29 +1,101 @@
 # Import socket module
 import json
 import socket
+import sys
+import threading
+
+from PacketHandler import HandshakePacket, BadPacket
+from PacketHandler import ClientPacket
+
+
+class ThreadMessage:
+    handled = True
+    message = ''
+
+
+class KeyboardThread(threading.Thread):
+
+    def __init__(self, input_cbk=None, name='keyboard-input-thread'):
+        self.input_cbk = input_cbk
+        super(KeyboardThread, self).__init__(name=name)
+        self.start()
+
+    def run(self):
+        while True:
+            self.input_cbk(input())
+
+
+KeyboardMessage = ThreadMessage
+
+
+def process(inp):
+    global KeyboardMessage
+    KeyboardMessage.message = inp
+    KeyboardMessage.handled = False
 
 
 def Handshake(connection):
-    ip_port = connection.getsockname()
-    HandshakeTest = {
-        'command': 'hello',
-        'host': ip_port[0],
-        'port': ip_port[1],
-        'username': 'Alexey',
-        'message': 'Henlo',
-        'destination': 'David',
-        'processed': True
-    }
-    data = json.dumps(HandshakeTest)
-    connection.send(data.encode('UTF-8'))
+    ip_port = connection.getsockname()  # get ip:port data, to be used in json
+    HSPacket = HandshakePacket(JsonData=None)  # create a packet with default handhsake settings
+    HSPacket.LoadIp(ip_port)  # load the ip of the client
+    data = HSPacket.DumpJson()  # dump content to transmittable json type
+    connection.send(data)  # greeting message to server, command is hello
+    ResponsePacket = HandshakePacket(connection.recv(1024))  # get the reply from server
+    HSPacket.command = 'read_back'  # update initial packet command
+
+    ReadBackCorrect = (ResponsePacket == HSPacket)  # compare packages for errors
+    if ReadBackCorrect:
+        HSPacket.command = 'ok'  # send the all_clear
+        data = HSPacket.DumpJson()
+        connection.send(data)
+    else:
+        return lambda x: BadPacket(JsonData=None)  # fail
+    HSPacket.LoadJson(connection.recv(1024))  # get the all-clear from server
+    if HSPacket.command == 'ready':
+        return HSPacket  # handshake ends
+    return lambda x: BadPacket(JsonData=None)  # fallback return
+
+
+def Dialogue(connection, DialoguePacket):
+
+    # usr = input('Please enter username: ')
+    # dest = input('Please enter destination username: ')
+    DialoguePacket.username = 'Alexey'  # TODO: unhardcode
+    DialoguePacket.destination = 'Danny'
+    KeyboardThread(process)
+    IsConnected = True
+    global KeyboardMessage
+    print('>', end='')
+    while IsConnected:
+        if not KeyboardMessage.handled:
+            DialoguePacket.message = KeyboardMessage.message
+            DialoguePacket.command = "send"
+            DialoguePacket.processed = False
+            KeyboardMessage.handled = True
+            connection.send(DialoguePacket.DumpJson())
+            DialoguePacket.LoadJson(connection.recv(1024))
+            if DialoguePacket.command == 'sent' and DialoguePacket.processed:
+                print('/rReceived./n>')
+
 
 def client():
-    host = '127.0.0.1'
-    port = 8080
-    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection.connect((host, port))
-    Handshake(connection)
-    connection.close()
+    HOSTNAME = ("127.0.0.1", 8080)  # hostname and port go here
+    ClientConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # init connection
+    DialoguePacket = False
+    while not DialoguePacket:
+        try:  # try to connect, if you can't, shutdown
+            ClientConnection.connect(HOSTNAME)
+        except ConnectionRefusedError:
+            print("Server may be down! Aborting...")
+            sys.exit(1)  # c-style error exit code
+        else:
+            DialoguePacket = Handshake(ClientConnection)  # go ahead and check if the handshake went all right
+            if not DialoguePacket:
+                ClientConnection.close()  # if the pipe is broken, reset connection and try again
+
+    Dialogue(ClientConnection, DialoguePacket)
+
+    ClientConnection.close()
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,114 +1,83 @@
 import socket
-from _thread import *
 import threading
 import json
-from datetime import datetime
-from cerberus import Validator
+from PacketHandler import PacketFormatValidator
+from PacketHandler import HandshakePacket
+from PacketHandler import ClientPacket
 
-ThreadLock = threading.Lock()
-
-
-class ClientData:
-    hostname = '0.0.0.0'
-    port = 8000
-    username = 'default'
-    message = 'placeholder'
-    command = 'default'
-    destination = 'someone else'
-
-    def load(self, json_data):
-        self.hostname = json_data['host']
-        self.port = json_data['port']
-        self.username = json_data['usr']
-        self.message = json_data['message']
-        self.command = json_data['command']
-        self.destination = json_data['destination']
-
-    def __str__(self):
-        return self.hostname + '\n' + str(self.port) + '\n' + self.username
-
-
-def greeting(host, port, username):
-    json_data = {
-        'command': 'ReadBack',
-        'host': host,
-        'port': port,
-        'usr': username
-    }
-    return json.dumps(json_data)  # returns a string
+routes = {}  # routes dict, format user: (ip,port)
 
 
 def Handshake(connection):
-    client = ClientData()
-    HandshakeSchema = {
-        'command': {'type': 'string'},
-        'host': {'type': 'string'},
-        'port': {'type': 'int'},
-        'username': {'type': 'string'},
-        'message': {'type': 'string'},
-        'destination': {'type': 'string'}
-    }
-    v = Validator()
-
-    data = json.loads(connection.recv(1024))
-    if not (v.validate(data, HandshakeSchema) and data['command'] == 'hello'):
+    HSPacket = HandshakePacket(connection.recv(1024))  # load json from recvd packet
+    if not (PacketFormatValidator(HSPacket) and HSPacket.command == 'hello'):
         connection.send(b"Malformed input!")
+        print("Broken connection! Sending abort message...")
         return False
     else:
-        client.load(data)
-        return client
+        HSPacket.command = 'read_back'
+        connection.send(HSPacket.DumpJson())
+        HSPacket.LoadJson(connection.recv(1024))
+        if HSPacket.command == 'ok':
+            print(f"Handshake established with {HSPacket.hostname}: {HSPacket.port}")
+            HSPacket.command = 'ready'
+            connection.send(HSPacket.DumpJson())
+            return HSPacket
+        else:
+            return False
 
 
-def ClientThread(connection):
-    IsConnected = True
-    ConnectionExitedOk = None
-    # HandShake
-    # if Handshake(connection):
-    #     IsConnected = True
-    # else:
-    #     ConnectionExitedOk = False
-    #     ThreadLock.release()
-    #data = json.loads(connection.recv(1024))
-    data = connection.recv(1024)
-    print(data)
-    # while IsConnected:
-    #     data = json.loads(connection.recv(1024))
-    #     data = connection.recv(1024)
-    #     print(data)
-    #     if data['command'] == 'end':
-    #         IsConnected = False
-    #         ConnectionExitedOk = True
-    #         ThreadLock.release()
-
-
-def Main():
-    host = ""
-
-    # reverse a port on your computer
-    # in our case it is 12345 but it
-    # can be anything
-    port = 12345
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    print("socket binded to port", port)
-
-    # put the socket into listening mode
-    s.listen(5)
-    print("socket is listening")
-
-    # a forever loop until client wants to exit
+def Dialogue(connection):
+    ChatPacket = ClientPacket(JsonData=None)
+    data = 0
     while True:
-        # establish connection with client
-        c, addr = s.accept()
+        ChatPacket.LoadJson(connection.recv(1024))
+        if ChatPacket.command == 'send':
+            try:
+                DestinationSocket = routes[ChatPacket.destination]
+            except KeyError:
+                ChatPacket.command = 'usrfail'
+                ChatPacket.processed = True
+                connection.send(ChatPacket.DumpJson())
+            else:
+                DestinationSocket.send(ChatPacket.DumpJson())
+                data = DestinationSocket.recv(1024)
+                print(data)
+                ChatPacket.LoadJson(data)
+                if ChatPacket.processed:
+                    ChatPacket.command = 'sent'
+                    connection.send(ChatPacket.DumpJson())
 
-        # lock acquired by client
-        ThreadLock.acquire()
-        print('Connected to :', addr[0], ':', addr[1])
 
-        # Start a new thread and return its identifier
-        start_new_thread(ClientThread, (c,))
-    s.close()
+
+
+class ClientThread(threading.Thread):
+    def __init__(self, Address, ClientSocket):
+        threading.Thread.__init__(self)
+        self.socket = ClientSocket
+        self.address = Address
+
+    def run(self):
+        user = Handshake(self.socket)
+        if user:
+            routes[user.username] = self.socket
+        Dialogue(self.socket)
+
+
+def main():
+    LOCALHOST = "127.0.0.1"
+    PORT = 8080
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((LOCALHOST, PORT))
+    print("Server started")
+    print("Waiting for client request..")
+    while True:
+        server.listen(1)
+        ClientSocket, clientAddress = server.accept()
+        NewThread = ClientThread(clientAddress, ClientSocket)
+        NewThread.start()
 
 
 if __name__ == '__main__':
-    Main()
+    main()
